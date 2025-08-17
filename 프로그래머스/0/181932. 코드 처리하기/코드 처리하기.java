75d2e04c522442fca6b84cfd1d33257983be917e
HG-Yoon <younha3710@naver.com>
2025-08-18 00:45:58 +0900

    [level 0] Title: 코드 처리하기, Time: 9.11 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -1,26 +1,18 @@
 class Solution {
         public String solution(String code) {
-            String ret = "";
-            String[] code_idx = code.split("");
-            for (int idx = 0, mode = 0; idx < code_idx.length; idx++) {
-                boolean code_eq_1 = code_idx[idx].equals("1");
+            StringBuilder anwer = new StringBuilder();
+            int mode = 0;
+            for (int i = 0; i < code.length(); i++) {
+                char current = code.charAt(i);
+                if (current == '1') {
+                    mode = mode == 0 ? 1 : 0;
+                    continue;
+                }
 
-                if (mode == 0) {
-                    if (!code_eq_1 && idx % 2 == 0) {
-                        ret += code_idx[idx];
-                    }
-                    if (code_eq_1) {
-                        mode = 1;
-                    }
-                } else {
-                    if (!code_eq_1 && idx % 2 != 0) {
-                        ret += code_idx[idx];
-                    }
-                    if (code_eq_1) {
-                        mode = 0;
-                    }
+                if (i % 2 == mode) {
+                    anwer.append(current);
                 }
             }
-            return ret == "" ? "EMPTY" : ret;
+            return anwer.length() == 0 ? "EMPTY" : anwer.toString();
         }
     }
