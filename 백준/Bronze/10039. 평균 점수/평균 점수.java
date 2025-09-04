ff0dd67537a18c7d7b1e8455968f68302c9b061f
HG-Yoon <younha3710@naver.com>
2025-09-05 00:08:10 +0900

    [Bronze IV] Title: 평균 점수, Time: 104 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Bronze/10039. 평균 점수/평균 점수.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int[] scores = new int[5];
+        int sum = 0;
+        for (int i = 0; i < 5; i++) {
+            scores[i] = Integer.parseInt(br.readLine());
+            if (scores[i] < 40) {
+                scores[i] = 40;
+            }
+            sum += scores[i];
+        }
+        int avg = sum / 5;
+        bw.write(String.valueOf(avg));
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
