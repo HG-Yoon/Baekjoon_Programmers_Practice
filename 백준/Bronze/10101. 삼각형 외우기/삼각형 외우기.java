4ca4d35c0e3969d3d6c8b4bd8a2f9a75ea72ff7c
HG-Yoon <younha3710@naver.com>
2025-09-07 15:28:54 +0900

    [Bronze IV] Title: 삼각형 외우기, Time: 100 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.java
@@ -0,0 +1,27 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+
+        if (a == 60 && b == 60 && c == 60) {
+            bw.write("Equilateral");
+        } else if (a + b + c == 180 && ((a == b && a != c) || (a == c && a != b) || (b == c && a != b))) {
+            bw.write("Isosceles");
+        } else if (a + b + c == 180 && (a != b && b != c)) {
+            bw.write("Scalene");
+        } else if (a + b + c != 180) {
+            bw.write("Error");
+        }
+        
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
