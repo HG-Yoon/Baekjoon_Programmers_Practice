1bc74536f59564fafab0918c713d212ce527fc7f
HG-Yoon <younha3710@naver.com>
2025-09-09 01:24:23 +0900

    [Bronze IV] Title: 모음의 개수, Time: 104 ms, Memory: 14072 KB -BaekjoonHub

---
+++ 백준/Bronze/1264. 모음의 개수/모음의 개수.java
@@ -0,0 +1,28 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        while (true) {
+            String s = br.readLine();
+            if (s.equals("#")) {
+                break;
+            }
+            int count = 0;
+            for (char c : s.toCharArray()) {
+                if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+                        c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U') {
+                    count++;
+                }
+            }
+            bw.write(String.valueOf(count) + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
