3cd73fe92b4b143091958c2efede8895faf3def6
HG-Yoon <younha3710@naver.com>
2025-09-10 18:24:55 +0900

    [Bronze V] Title: 윤년, Time: 104 ms, Memory: 14288 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int y = Integer.parseInt(br.readLine());
+        bw.write(((y % 4 == 0 && y % 100 != 0) || y % 400 == 0) ? "1" : "0");
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
