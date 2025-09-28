27b1e7977aca12f454a6a273cbfd8250d1399d33
HG-Yoon <younha3710@naver.com>
2025-09-29 02:02:17 +0900

    [Bronze IV] Title: 시험 점수, Time: 100 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Bronze/5596. 시험 점수/시험 점수.java
+++ 백준/Bronze/5596. 시험 점수/시험 점수.java
@@ -1,5 +1,5 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
 
@@ -7,23 +7,23 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int[] minGukScores = new int[4];
-        int[] manSeScores = new int[4];
+        int[] minGuk = new int[4];
+        int[] manSae = new int[4];
         int s = 0, t = 0;
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < 4; i++) {
-            minGukScores[i] = Integer.parseInt(st.nextToken());
-            s += minGukScores[i];
+            minGuk[i] = Integer.parseInt(st.nextToken());
+            s += minGuk[i];
         }
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < 4; i++) {
-            manSeScores[i] = Integer.parseInt(st.nextToken());
-            t += manSeScores[i];
+            manSae[i] = Integer.parseInt(st.nextToken());
+            t += manSae[i];
         }
-        if (s != t) {
-            bw.write(String.valueOf(Math.max(s, t)));
-        } else {
+        if (s >= t) {
             bw.write(String.valueOf(s));
+        } else {
+            bw.write(String.valueOf(t));
         }
 
         bw.flush();
