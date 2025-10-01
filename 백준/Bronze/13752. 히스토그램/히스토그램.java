3b780b76f5958ae82ec16708e6b3265ea4ca7e3f
HG-Yoon <younha3710@naver.com>
2025-10-02 05:24:40 +0900

    [Bronze IV] Title: 히스토그램, Time: 108 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/13752. 히스토그램/히스토그램.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            int k = Integer.parseInt(br.readLine());
+            for (int j = 1; j <= k; j++) {
+                bw.write("=");
+            }
+            bw.write("\n");
+        }
+        
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
