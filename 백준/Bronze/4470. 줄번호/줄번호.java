2cfae3e4a91a6ede0717d1676cfaf0366afc1fb2
HG-Yoon <younha3710@naver.com>
2025-10-02 05:14:09 +0900

    [Bronze IV] Title: 줄번호, Time: 124 ms, Memory: 15952 KB -BaekjoonHub

---
+++ 백준/Bronze/4470. 줄번호/줄번호.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            String s = br.readLine();
+            bw.write(String.valueOf(i) + ". " + s + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
