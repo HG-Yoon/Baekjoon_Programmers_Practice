05d2f8c8dd2093f21a1028a4ba7629d760d093ee
HG-Yoon <younha3710@naver.com>
2025-10-02 00:16:28 +0900

    [Bronze IV] Title: 와글와글 숭고한, Time: 104 ms, Memory: 14092 KB -BaekjoonHub

---
+++ 백준/Bronze/17388. 와글와글 숭고한/와글와글 숭고한.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int s = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
+        int sum = s + k + h;
+        int min = Math.min(Math.min(s, k), h);
+        if (sum >= 100) {
+            bw.write("OK");
+        } else {
+            if (min == s) {
+                bw.write("Soongsil");
+            } else if (min == k) {
+                bw.write("Korea");
+            } else if (min == h) {
+                bw.write("Hanyang");
+            }
+        }
+        
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
