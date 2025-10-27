535855485544ce2ced09b1cdeae5d2dd9339c003
HG-Yoon <younha3710@naver.com>
2025-10-28 01:30:35 +0900

    [Bronze IV] Title: 녹색거탑, Time: 96 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Bronze/24723. 녹색거탑/녹색거탑.java
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.java
@@ -8,8 +8,9 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
-        bw.write(String.valueOf((int) Math.pow(2, n)));
-
+        int answer = (int) Math.pow(2, n);
+        bw.write(String.valueOf(answer));
+        
         bw.flush();
         bw.close();
         br.close();
