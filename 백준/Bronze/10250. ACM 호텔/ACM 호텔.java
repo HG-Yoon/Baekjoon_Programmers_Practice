e036ebafdf578039d2f3a39882d3917bc1507d51
HG-Yoon <younha3710@naver.com>
2025-11-01 04:32:07 +0900

    [Bronze III] Title: ACM 호텔, Time: 128 ms, Memory: 14808 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
@@ -10,16 +10,12 @@ public class Main {
         int t = Integer.parseInt(br.readLine());
         for (int i = 0; i < t; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int h = Integer.parseInt(st.nextToken()); // 호텔의 층 수
-            int w = Integer.parseInt(st.nextToken()); // 각 층의 방 수
-            int n = Integer.parseInt(st.nextToken()); // 몇 번째 손님
-            int floor = (n % h == 0) ? h : n % h;
+            int h = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            int n = Integer.parseInt(st.nextToken());
+            int floor = n % h == 0 ? h : n % h;
             int room = (n - 1) / h + 1;
-            if (room < 10) {
-                bw.write(String.valueOf(floor) + "0" + String.valueOf(room) + "\n");
-            } else {
-                bw.write(String.valueOf(floor) + String.valueOf(room) + "\n");
-            }
+            bw.write(String.format("%d%02d\n", floor, room));
         }
 
         bw.flush();
