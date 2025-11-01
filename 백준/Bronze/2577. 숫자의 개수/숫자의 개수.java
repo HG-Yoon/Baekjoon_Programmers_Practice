9d84b01ea62452655421dcd727ec6038738af8ba
HG-Yoon <younha3710@naver.com>
2025-11-02 00:20:39 +0900

    [Bronze II] Title: 숫자의 개수, Time: 100 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
@@ -7,14 +7,16 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int a = Integer.parseInt(br.readLine());
-        int b = Integer.parseInt(br.readLine());
-        int c = Integer.parseInt(br.readLine());
-        int abc = a * b * c;
-        String abcStr = String.valueOf(abc);
+        long a = Long.parseLong(br.readLine());
+        long b = Long.parseLong(br.readLine());
+        long c = Long.parseLong(br.readLine());
+
+        long abc = a * b * c;
         int[] cnt = new int[10];
-        for (int i = 0; i < abcStr.length(); i++) {
-            cnt[abcStr.charAt(i) - '0']++;
+        String abcS = String.valueOf(abc);
+        for (int i = 0; i < abcS.length(); i++) {
+            char ch = abcS.charAt(i);
+            cnt[ch - '0']++;
         }
         for (int i = 0; i < cnt.length; i++) {
             bw.write(String.valueOf(cnt[i]) + "\n");
