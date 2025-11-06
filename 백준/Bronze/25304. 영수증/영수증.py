ddb373dfb0f8331c30d84e161852a8b48a2721bf
HG-Yoon <younha3710@naver.com>
2025-11-06 14:59:14 +0900

    [Bronze IV] Title: 영수증, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+print = sys.stdout.write
+
+x = int(input())
+n = int(input())
+sum = 0
+for i in range(n):
+    a, b = map(int, input().split())
+    sum += a*b
+
+if sum == x:
+    print("Yes")
+else:
+    print("No")
