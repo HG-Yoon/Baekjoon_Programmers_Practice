c5b56ea0b4f6f85db2ae9c6525332dc7045271db
HG-Yoon <younha3710@naver.com>
2025-11-15 04:51:00 +0900

    [Bronze II] Title: 16진수, Time: 100 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/1550. 16진수/16진수.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String hexString = br.readLine();
+        bw.write(String.valueOf(Integer.parseInt(hexString, 16)));
+        
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
