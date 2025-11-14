7f8cb4be891eef3f4c1e1f006a7f372647ae1635
HG-Yoon <younha3710@naver.com>
2025-11-15 04:25:47 +0900

    Refactor to use HashSet for unique remainders

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -7,18 +7,12 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int[] arr = new int[42];
-        int x;
-        int cnt = 0;
+        HashSet<Integer> hashSet = new HashSet<>();
         for (int i = 0; i < 10; i++) {
-            x = Integer.parseInt(br.readLine());
-            arr[x % 42] = 1;
+            hashSet.add(Integer.parseInt(br.readLine()) % 42);
         }
-        for (int i = 0; i < 42; i++) {
-            cnt += arr[i];
-        }
-        bw.write(Integer.toString(cnt));
-
+        bw.write(String.valueOf(hashSet.size()));
+        
         bw.flush();
         bw.close();
         br.close();
