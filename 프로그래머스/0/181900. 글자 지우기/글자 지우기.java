9adc14fde5b8745eab8269e45e819fcbc1c3a380
HG-Yoon <younha3710@naver.com>
2025-11-22 01:59:42 +0900

    [level 0] Title: 글자 지우기, Time: 0.16 ms, Memory: 90.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public String solution(String my_string, int[] indices) {
+        StringBuilder sb = new StringBuilder();
+        Set<Integer> set = new HashSet<Integer>();
+        for (int i = 0; i < indices.length; i++) {
+            set.add(indices[i]);
+        }
+        for (int i = 0; i < my_string.length(); i++) {
+            if (!set.contains(i)) {
+                sb.append(my_string.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+}
