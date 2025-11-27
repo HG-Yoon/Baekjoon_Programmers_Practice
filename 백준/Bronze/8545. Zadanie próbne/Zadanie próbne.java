fa0ac693b0ffa584a0d1ba4c2db072bc2521f8a9
HG-Yoon <younha3710@naver.com>
2025-11-27 19:55:16 +0900

    [Bronze V] Title: Zadanie próbne, Time: 100 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Bronze/8545. Zadanie próbne/Zadanie próbne.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String s = br.readLine();
+        StringBuilder sb = new StringBuilder(s);
+        bw.write(sb.reverse().toString());
+        
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
