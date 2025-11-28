a767e3bdb2202fc4c7e1b9a3e62256f7427f18b8
HG-Yoon <younha3710@naver.com>
2025-11-29 01:06:13 +0900

    [Bronze V] Title: 가희와 교통 요금, Time: 108 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/34665. 가희와 교통 요금/가희와 교통 요금.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String a = br.readLine();
+        String b = br.readLine();
+        if (a.equals(b)) {
+            bw.write("0");
+        } else {
+            bw.write("1550");
+        }
+        
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
