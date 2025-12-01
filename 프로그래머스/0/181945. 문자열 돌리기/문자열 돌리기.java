f99396ab41339d77afbd07a4ec0a3d1853f714b2
HG-Yoon <younha3710@naver.com>
2025-12-01 19:04:00 +0900

    [level 0] Title: 문자열 돌리기, Time: 172.97 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.java
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.java
@@ -4,7 +4,8 @@ public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String a = sc.next();
-        for (char c : a.toCharArray()) {
+        for (int i = 0; i < a.length(); i++) {
+            char c = a.charAt(i);
             System.out.println(c);
         }
     }
