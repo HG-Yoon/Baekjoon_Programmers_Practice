362e698c95a47b041b641eebd3e7ab3e800bdba4
HG-Yoon <younha3710@naver.com>
2025-12-01 19:24:03 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 17.08 ms, Memory: 91.8 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -1,10 +1,9 @@
 class Solution {
-        public String solution(String my_string, String overwrite_string, int s) {
-            String answer = "";
-            answer = my_string.substring(0, s) + overwrite_string;
-            if (my_string.length() > overwrite_string.length()) {
-                answer += my_string.substring(answer.length());
-            }
-            return answer;
-        }
+    public String solution(String my_string, String overwrite_string, int s) {
+        String answer = "";
+        String part1 = my_string.substring(0, s);
+        String part2 = my_string.substring(s + overwrite_string.length());
+        answer = part1 + overwrite_string + part2;
+        return answer;
     }
+}
