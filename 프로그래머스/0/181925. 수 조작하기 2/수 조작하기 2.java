d78810b6392e7c5c4c336db5803ea36437831ee8
HG-Yoon <younha3710@naver.com>
2025-12-03 20:00:15 +0900

    [level 0] Title: 수 조작하기 2, Time: 9.02 ms, Memory: 90 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -1,17 +1,20 @@
 class Solution {
-        public String solution(int[] numLog) {
-            String answer = "";
-            for (int i = 1; i <= numLog.length - 1; i++) {
-                if (numLog[i] - numLog[i - 1] == 1) {
-                    answer += "w";
-                } else if (numLog[i] - numLog[i - 1] == -1) {
-                    answer += "s";
-                } else if (numLog[i] - numLog[i - 1] == 10) {
-                    answer += "d";
-                } else if (numLog[i] - numLog[i - 1] == -10) {
-                    answer += "a";
-                }
+    public String solution(int[] numLog) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < numLog.length; i++) {
+            if (numLog[i] == numLog[i - 1] + 1) {
+                sb.append("w");
             }
-            return answer;
+            if (numLog[i] == numLog[i - 1] - 1) {
+                sb.append("s");
+            }
+            if (numLog[i] == numLog[i - 1] + 10) {
+                sb.append("d");
+            }
+            if (numLog[i] == numLog[i - 1] - 10) {
+                sb.append("a");
+            }
         }
+        return sb.toString();
     }
+}
