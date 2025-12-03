27d56ebf28359f6b199db666c60acb3b27e1bbbd
HG-Yoon <younha3710@naver.com>
2025-12-03 14:12:31 +0900

    [level 0] Title: 마지막 두 원소, Time: 4.53 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -1,17 +1,20 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 class Solution {
-        public int[] solution(int[] num_list) {
-            int[] answer = new int[num_list.length + 1];
-            if (num_list[num_list.length - 1] > num_list[num_list.length - 2]) {
-                for (int i = 0; i < answer.length - 1; i++) {
-                    answer[i] = num_list[i];
-                }
-                answer[answer.length - 1] = num_list[num_list.length - 1] - num_list[num_list.length - 2];
-            } else {
-                for (int i = 0; i < answer.length - 1; i++) {
-                    answer[i] = num_list[i];
-                }
-                answer[answer.length - 1] = num_list[num_list.length - 1] * 2;
-            }
-            return answer;
+    public int[] solution(int[] num_list) {
+        List<Integer> IntegerNumList = Arrays.stream(num_list)
+                .boxed()
+                .collect(Collectors.toList());
+        if (num_list[num_list.length - 1] > num_list[num_list.length - 2]) {
+            IntegerNumList.add(num_list[num_list.length - 1] - num_list[num_list.length - 2]);
+        } else {
+            IntegerNumList.add(num_list[num_list.length - 1] * 2);
         }
+        int[] answer = IntegerNumList.stream()
+                .mapToInt(i -> i)
+                .toArray();
+        return answer;
     }
+}
