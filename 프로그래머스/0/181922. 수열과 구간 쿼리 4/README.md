7fa75bd56cf79fe3d115430e1ddf7dab55225233
HG-Yoon <younha3710@naver.com>
2025-12-04 19:30:31 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 2.78 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/0/181922. 수열과 구간 쿼리 4/README.md
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/README.md
@@ -1,10 +1,10 @@
 # [level 0] 수열과 구간 쿼리 4 - 181922 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181922) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181922#) 
 
 ### 성능 요약
 
-메모리: 88.4 MB, 시간: 1.83 ms
+메모리: 72.7 MB, 시간: 2.78 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 28일 00:01:18
+2025년 12월 04일 19:30:24
 
 ### 문제 설명
 
--- 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.java
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.java
@@ -1,17 +1,17 @@
 class Solution {
-        public int[] solution(int[] arr, int[][] queries) {
-            int[] answer = new int[arr.length];
-            for (int[] query : queries) {
-                int s = query[0];
-                int e = query[1];
-                int k = query[2];
-                for (int i = s; i <= e; i++) {
-                    if (i % k == 0) {
-                        arr[i] += 1;
-                    }
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = {};
+        for (int i = 0; i < queries.length; i++) {
+            int s = queries[i][0];
+            int e = queries[i][1];
+            int k = queries[i][2];
+            for (int j = s; j <= e; j++) {
+                if (j % k == 0) {
+                    arr[j] += 1;
                 }
             }
-            answer = arr.clone();
-            return answer;
         }
+        answer = arr;
+        return answer;
     }
+}
