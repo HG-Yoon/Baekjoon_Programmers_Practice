8639d37563e0382b96235ec8dcde1d5a3e4b6982
HG-Yoon <younha3710@naver.com>
2025-12-04 20:16:01 +0900

    [level 0] Title: 카운트 업, Time: 6.73 ms, Memory: 89.4 MB -BaekjoonHub

--- 프로그래머스/0/181920. 카운트 업/카운트 업.java
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.java
@@ -1,11 +1,15 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 class Solution {
-        public int[] solution(int start_num, int end_num) {
-            List<Integer> result = new ArrayList<>();
-            for (int i = start_num; i <= end_num; i++) {
-                result.add(i);
-            }
-            return result.stream().mapToInt(i -> i).toArray();
+    public int[] solution(int start_num, int end_num) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = start_num; i <= end_num; i++) {
+            list.add(i);
         }
+        int[] answer = list.stream()
+                .mapToInt(i -> i)
+                .toArray();
+        return answer;
     }
+}
