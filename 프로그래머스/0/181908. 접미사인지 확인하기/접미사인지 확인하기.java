5647e902949f7cac406e54ae6126c1807ce7f493
HG-Yoon <younha3710@naver.com>
2025-12-06 19:36:38 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.06 ms, Memory: 85.7 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -1,15 +1,5 @@
 class Solution {
-        public int solution(String my_string, String is_suffix) {
-            int answer = 0;
-            String[] suffix = new String[my_string.length()];
-            for (int i = 0; i < my_string.length(); i++) {
-                suffix[i] = my_string.substring(i);
-            }
-            for (int i = 0; i < suffix.length; i++) {
-                if (is_suffix.equals(suffix[i])) {
-                    answer = 1;
-                }
-            }
-            return answer;
-        }
+    public int solution(String my_string, String is_suffix) {
+        return my_string.endsWith(is_suffix) ? 1 : 0;
     }
+}
