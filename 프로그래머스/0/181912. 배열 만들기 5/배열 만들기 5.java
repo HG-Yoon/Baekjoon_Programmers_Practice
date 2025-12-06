e7c96d9bd59e6289eaafab072770610faa8f8841
HG-Yoon <younha3710@naver.com>
2025-12-06 18:52:20 +0900

    [level 0] Title: 배열 만들기 5, Time: 9.09 ms, Memory: 89.7 MB -BaekjoonHub

--- 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.java
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.java
@@ -1,17 +1,19 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 class Solution {
-        public int[] solution(String[] intStrs, int k, int s, int l) {
-            int[] answer = {};
-            String[] ret = new String[intStrs.length];
-            Stack<Integer> stack = new Stack<>();
-            for (int i = 0; i < intStrs.length; i++) {
-                ret[i] = intStrs[i].substring(s, s + l);
-                if (Integer.parseInt(ret[i]) > k) {
-                    stack.add(Integer.parseInt(ret[i]));
-                }
+    public int[] solution(String[] intStrs, int k, int s, int l) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < intStrs.length; i++) {
+            String subStr = intStrs[i].substring(s, s + l);
+            int intSubStr = Integer.parseInt(subStr);
+            if (intSubStr > k) {
+                list.add(intSubStr);
             }
-            answer = stack.stream().mapToInt(Integer::intValue).toArray();
-            return answer;
         }
+        int[] answer = list.stream()
+                .mapToInt(Integer::intValue)
+                .toArray();
+        return answer;
     }
+}
