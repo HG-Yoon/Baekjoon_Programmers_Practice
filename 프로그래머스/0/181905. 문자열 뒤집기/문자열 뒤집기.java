758ba33cab5c10df51ee792c607160d3f02b8ab2
HG-Yoon <younha3710@naver.com>
2025-12-07 22:52:38 +0900

    [level 0] Title: 문자열 뒤집기, Time: 18.62 ms, Memory: 91.5 MB -BaekjoonHub

--- 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,12 +1,12 @@
 class Solution {
-        public String solution(String my_string, int s, int e) {
-            String answer = "";
-            String prefix = my_string.substring(0, s);
-            String to_reverse = my_string.substring(s, e + 1);
-            String suffix = my_string.substring(e + 1);
-            StringBuilder sb = new StringBuilder(to_reverse);
-            String reversed_part = sb.reverse().toString();
-            answer = prefix + reversed_part + suffix;
-            return answer;
-        }
+    public String solution(String my_string, int s, int e) {
+        String answer = "";
+        String prefix = my_string.substring(0, s);
+        String part = my_string.substring(s, e + 1);
+        StringBuilder sb = new StringBuilder(part);
+        String reversedStr = sb.reverse().toString();
+        String suffix = my_string.substring(e + 1, my_string.length());
+        answer = prefix + reversedStr + suffix;
+        return answer;
     }
+}
