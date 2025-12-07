a3cf8b67472314ba5694093a51392d313510f9ae
HG-Yoon <younha3710@naver.com>
2025-12-07 22:40:52 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.05 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -1,15 +1,5 @@
 class Solution {
-        public int solution(String my_string, String is_prefix) {
-            int answer = 0;
-            String[] prefixes = new String[my_string.length()];
-            for (int i = 0; i < my_string.length(); i++) {
-                prefixes[i] = my_string.substring(0, i + 1);
-            }
-            for (int i = 0; i < prefixes.length; i++) {
-                if (is_prefix.equals(prefixes[i])) {
-                    answer = 1;
-                }
-            }
-            return answer;
-        }
+    public int solution(String my_string, String is_prefix) {
+        return my_string.startsWith(is_prefix) ? 1 : 0;
     }
+}
