50b16379f0ab3aba0579da6992c142ac3aa553df
HG-Yoon <younha3710@naver.com>
2025-12-08 03:33:43 +0900

    [level 0] Title: 글자 지우기, Time: 0.15 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -1,13 +1,18 @@
-import java.util.*;
+import java.util.HashSet;
+import java.util.Set;
+
 class Solution {
     public String solution(String my_string, int[] indices) {
-        StringBuilder sb = new StringBuilder();
-        Set<Integer> set = new HashSet<Integer>();
-        for (int i = 0; i < indices.length; i++) {
-            set.add(indices[i]);
+        // 제거할 인덱스를 Set에 저장
+        Set<Integer> removeSet = new HashSet<>();
+        for (int idx : indices) {
+            removeSet.add(idx);
         }
+        // StringBuilder로 결과 문자열 생성
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < my_string.length(); i++) {
-            if (!set.contains(i)) {
+            // 제거 대상이 아니면 추가
+            if (!removeSet.contains(i)) {
                 sb.append(my_string.charAt(i));
             }
         }
