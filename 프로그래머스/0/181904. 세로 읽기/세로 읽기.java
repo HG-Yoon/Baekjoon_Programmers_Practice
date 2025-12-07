25e86e008844979474b47caa4c73de345e587c3c
HG-Yoon <younha3710@naver.com>
2025-12-07 23:20:11 +0900

    [level 0] Title: 세로 읽기, Time: 0.12 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
@@ -1,19 +1,11 @@
 class Solution {
     public String solution(String my_string, int m, int c) {
-        StringBuilder answer = new StringBuilder();
-        int row = my_string.length() / m;
-        int col = m;
-        char[][] matrix = new char[row][col];
-        int index = 0;
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                matrix[i][j] = my_string.charAt(index++);
-            }
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < my_string.length() / m; i++) {
+            sb.append(my_string.charAt(i * m + (c - 1)));
         }
-        for (int i = 0; i < row; i++) {
-            answer.append(matrix[i][c - 1]);
-        }
-
-        return answer.toString();
+        answer = sb.toString();
+        return answer;
     }
 }
