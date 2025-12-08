bc7ecdb9daa428749c621039ce2c39a3b4e14b17
HG-Yoon <younha3710@naver.com>
2025-12-08 20:24:25 +0900

    Refactor document search to use brute force method

--- 백준/Silver/1543. 문서 검색/문서 검색.java
+++ 백준/Silver/1543. 문서 검색/문서 검색.java
@@ -1,26 +1,36 @@
 import java.io.*;
 
 public class Main {
-
-    public static void main(String[] args) {
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));) {
-
-            String document = br.readLine();
-            String word = br.readLine();
-            int i = 0; // 문서 탐색 시작 위치
-            int count = 0;
-            while (i <= document.length() - word.length()) {
-                if (document.substring(i, i + word.length()).equals(word)) {
-                    count += 1; // 단어를 찾았으니 카운트 증가
-                    i += word.length(); // 겹치지 않게 다음 탐색은 단어 끝 이후부터
-                } else {
-                    i += 1; // 매칭되지 않으면 한 칸 앞으로 이동
+    public static int BruteForceSearch(String text, String pattern) {
+        int n = text.length();
+        int m = pattern.length();
+        int cnt = 0;
+        for (int i = 0; i <= n - m; i++) {
+            int j;
+            for (j = 0; j < m; j++) {
+                if (text.charAt(i + j) != pattern.charAt(j)) {
+                    break;
                 }
             }
-            bw.write(String.valueOf(count));
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+            if (j == m) {
+                cnt++;
+                i += m - 1;
+            }
         }
+        return cnt;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String document = br.readLine();
+        String word = br.readLine();
+        int answer = BruteForceSearch(document, word);
+        bw.write(String.valueOf(answer));
+
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
