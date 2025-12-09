e621baa3dfaa93cfc4e97465786a16477875ddbe
HG-Yoon <younha3710@naver.com>
2025-12-09 19:10:27 +0900

    [level 0] Title: 배열 만들기 3, Time: 15.97 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.java
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.java
@@ -1,20 +1,21 @@
-import java.util.*;
-import java.util.stream.*;
+import java.util.ArrayList;
+import java.util.List;
 
 class Solution {
     public int[] solution(int[] arr, int[][] intervals) {
         int[] answer = {};
-        List<Integer>list=Arrays.stream(answer)
-                                .boxed()
-                                .collect(Collectors.toList());
-        for(int i=intervals[0][0];i<=intervals[0][1];i++){
-            list.add(arr[i]);
+        List<Integer> temp = new ArrayList<>();
+        int a1 = intervals[0][0];
+        int b1 = intervals[0][1];
+        int a2 = intervals[1][0];
+        int b2 = intervals[1][1];
+        for (int i = a1; i <= b1; i++) {
+            temp.add(arr[i]);
         }
-        for(int i=intervals[1][0];i<=intervals[1][1];i++){
-            list.add(arr[i]);
+        for (int i = a2; i <= b2; i++) {
+            temp.add(arr[i]);
         }
-        answer=list.stream().mapToInt(i->i).toArray();
-            
+        answer = temp.stream().mapToInt(Integer::intValue).toArray();
         return answer;
     }
 }
