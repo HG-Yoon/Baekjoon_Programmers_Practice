51f130842e2ec129aa1428b0431f5cc5f113a4e2
HG-Yoon <younha3710@naver.com>
2025-12-27 06:30:32 +0900

    [level 0] Title: 1로 만들기, Time: 0.03 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        for (int i = 0; i < num_list.length; i++) {
+            int num = num_list[i];
+            int cnt = 0;
+            while (num != 1) {
+                if (num % 2 == 0) {
+                    num /= 2;
+                } else if (num % 2 != 0) {
+                    num = (num - 1) / 2;
+                }
+                cnt++;
+            }
+            answer += cnt;
+        }
+        return answer;
+    }
+}
