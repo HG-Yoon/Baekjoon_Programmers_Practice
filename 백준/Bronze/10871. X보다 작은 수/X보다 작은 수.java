294b3058677b6a268012835aff3bdd896cc53116
HG-Yoon <younha3710@naver.com>
2026-01-02 04:16:43 +0900

    [Bronze V] Title: X보다 작은 수, Time: 152 ms, Memory: 16296 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
@@ -1,25 +1,24 @@
 import java.io.*;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int x = Integer.parseInt(st.nextToken());
-        int[] a = new int[n];
+        int N = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
+
+        int[] A = new int[N];
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            a[i] = Integer.parseInt(st.nextToken());
-            if (a[i] < x) {
-                bw.write(String.valueOf(a[i]) + " ");
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+            if (A[i] < X) {
+                bw.write(String.valueOf(A[i]) + " ");
             }
         }
-
+        
         bw.flush();
         bw.close();
         br.close();
