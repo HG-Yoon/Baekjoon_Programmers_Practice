0923244f55c6761990ca469ef0bd12d902d17395
HG-Yoon <younha3710@naver.com>
2026-01-16 19:55:49 +0900

    [Bronze V] Title: 윤년, Time: 100 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -1,13 +1,20 @@
 import java.io.*;
 
 public class Main {
+    static int solution(int n) {
+        if ((n % 4 == 0 && n % 100 != 0) || (n % 400 == 0)) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int y = Integer.parseInt(br.readLine());
-        bw.write(((y % 4 == 0 && y % 100 != 0) || y % 400 == 0) ? "1" : "0");
+        bw.write(String.valueOf(solution(y)));
 
         bw.flush();
         bw.close();
