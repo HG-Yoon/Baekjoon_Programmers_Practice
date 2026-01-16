c04fe3c7c4d9df68a12e9e2278335acc4d9196b1
HG-Yoon <younha3710@naver.com>
2026-01-16 20:41:28 +0900

    [Bronze III] Title: 홀수, Time: 104 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Bronze/2576. 홀수/홀수.java
+++ 백준/Bronze/2576. 홀수/홀수.java
@@ -3,33 +3,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int[] arr = new int[7];
         List<Integer> odds = new ArrayList<>();
         for (int i = 0; i < 7; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-            if (arr[i] % 2 != 0) {
-                odds.add(arr[i]);
+            int num = Integer.parseInt(br.readLine());
+            if (num % 2 != 0) {
+                odds.add(num);
             }
         }
         if (odds.isEmpty()) {
             bw.write("-1");
         } else {
-            int sum = 0;
-            int min = odds.get(0);
+            int oddSum = 0;
+            int oddMin = odds.get(0);
             for (int i = 0; i < odds.size(); i++) {
-                sum += odds.get(i);
-                min = Math.min(min, odds.get(i));
+                oddSum += odds.get(i);
+                oddMin = Math.min(oddMin, odds.get(i));
             }
-            bw.write(String.valueOf(sum) + "\n" + String.valueOf(min));
+            bw.write(String.valueOf(oddSum) + "\n");
+            bw.write(String.valueOf(oddMin));
         }
         
         bw.flush();
         bw.close();
         br.close();
     }
+
+
 }
