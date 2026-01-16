1d08f614e335767f8fd927174b5480b3cefd4e14
HG-Yoon <younha3710@naver.com>
2026-01-16 20:05:58 +0900

    [Bronze IV] Title: 주사위 세개, Time: 100 ms, Memory: 14076 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.java
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -1,7 +1,22 @@
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
+    static int solution(int a, int b, int c) {
+        int answer = 0;
+        if (a == b && a == c) {
+            answer = 10000 + a * 1000;
+        } else if (a == b && a != c) {
+            answer = 1000 + a * 100;
+        } else if (a == c && a != b) {
+            answer = 1000 + a * 100;
+        } else if (b == c && b != a) {
+            answer = 1000 + b * 100;
+        } else if (a != b && a != c) {
+            answer = Math.max(Math.max(a, b), c) * 100;
+        }
+        return answer;
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -11,15 +26,7 @@ public class Main {
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
         int c = Integer.parseInt(st.nextToken());
-        if (a == c && b == c) {
-            bw.write(String.valueOf(10000 + a * 1000));
-        } else if ((a == b && b != c) || (a == c && a != b)) {
-            bw.write(String.valueOf(1000 + a * 100));
-        } else if (b == c && b != a) {
-            bw.write(String.valueOf(1000 + b * 100));
-        } else if (a != b && a != c && b != c) {
-            bw.write(String.valueOf(Math.max(Math.max(a, b), c) * 100));
-        }
+        bw.write(String.valueOf(solution(a, b, c)));
         
         bw.flush();
         bw.close();
