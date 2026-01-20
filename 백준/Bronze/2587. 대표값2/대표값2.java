f1ae4733aac70ef5b2f772d032f992a6e10f4210
HG-Yoon <younha3710@naver.com>
2026-01-20 20:52:56 +0900

    [Bronze II] Title: 대표값2, Time: 116 ms, Memory: 16140 KB -BaekjoonHub

---
+++ 백준/Bronze/2587. 대표값2/대표값2.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int[] arr = new int[5];
+        int sum = 0;
+        for (int i = 0; i < 5; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            sum += arr[i];
+        }
+        int avg = sum / 5;
+        Arrays.sort(arr);
+        int mid = arr[2];
+        bw.write(String.valueOf(avg) + "\n" + String.valueOf(mid));
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
