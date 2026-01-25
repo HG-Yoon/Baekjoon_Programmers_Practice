140b24e1a0f63df04eb413b774f57493f3b480bf
HG-Yoon <younha3710@naver.com>
2026-01-25 22:44:17 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 108 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -1,14 +1,14 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int N = Integer.parseInt(br.readLine());
-        for (int i = 0; i < N; i++) {
-            for (int j = N; j > i; j--) {
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            for (int j = n; j > i; j--) {
                 bw.write("*");
             }
             bw.write("\n");
