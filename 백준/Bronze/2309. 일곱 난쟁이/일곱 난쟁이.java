cbbb5dd23cc044e17fe275e905deda0ba7d07ad2
HG-Yoon <younha3710@naver.com>
2026-02-12 03:16:58 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 92 ms, Memory: 14376 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,27 +1,23 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        ArrayList<Integer> arr = new ArrayList<>(9);
-        int sum = 0;
-        for (int i = 0; i < 9; i++) {
-            int n = Integer.parseInt(br.readLine());
-            arr.add(n);
-            sum += arr.get(i);
-        }
-        int twoSum = sum - 100;
+    static List<Integer> solution(List<Integer> list) {
+        int totalSum = 0;
         boolean found = false;
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                if (i != j && arr.get(i) + arr.get(j) == twoSum) {
+        for (int n : list) {
+            totalSum += n;
+        }
+        int removeSum = totalSum - 100;
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = i + 1; j < list.size(); j++) {
+                if (list.get(i) + list.get(j) == removeSum) {
+                    list.remove(Integer.valueOf(list.get(j)));
+                    list.remove(Integer.valueOf(list.get(i)));
                     found = true;
-                    arr.remove(j);
-                    arr.remove(i);
                     break;
                 }
             }
@@ -29,10 +25,24 @@ public class Main {
                 break;
             }
         }
-        Collections.sort(arr);
-        for (int n : arr) {
+        Collections.sort(list);
+        return list;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        List<Integer> heights = new ArrayList<>();
+        for (int i = 0; i < 9; i++) {
+            int num = Integer.parseInt(br.readLine());
+            heights.add(num);
+        }
+        List<Integer> answer = solution(heights);
+        for (int n : answer) {
             bw.write(String.valueOf(n) + "\n");
         }
+        
         bw.flush();
         bw.close();
         br.close();
