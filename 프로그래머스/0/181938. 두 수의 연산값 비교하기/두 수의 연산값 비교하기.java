d012176db0c4440814db78e93bf4a231fe6a2b3f
HG-Yoon <younha3710@naver.com>
2026-02-25 20:46:51 +0900

    [level 0] Title: 두 수의 연산값 비교하기, Time: 1.92 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.java
+++ 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.java
@@ -1,13 +1,8 @@
 class Solution {
     public int solution(int a, int b) {
-        int answer = 0;
-        int ab = Integer.parseInt(String.valueOf(a) + String.valueOf(b));
-        int twoAb = 2 * a * b;
-        if (ab > twoAb || ab == twoAb) {
-            answer = ab;
-        } else {
-            answer = twoAb;
-        }
-        return answer;
+        int concatAb = Integer.parseInt(String.valueOf(a) + String.valueOf(b));
+        int doubleProduct = 2 * a * b;
+
+        return Math.max(concatAb, doubleProduct);
     }
 }
