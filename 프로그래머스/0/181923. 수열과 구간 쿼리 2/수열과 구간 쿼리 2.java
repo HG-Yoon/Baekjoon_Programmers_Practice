cc508532e4fc4c08fe7c60786bd6a8b6f6c08d64
HG-Yoon <younha3710@naver.com>
2026-02-28 15:24:32 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 7.98 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
@@ -4,25 +4,24 @@ import java.util.List;
 class Solution {
     public int[] solution(int[] arr, int[][] queries) {
         List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < queries.length; i++) {
-            int s = queries[i][0];
-            int e = queries[i][1];
-            int k = queries[i][2];
-            int min = Integer.MAX_VALUE;
-            for (int j = s; j <= e; j++) {
-                if (arr[j] > k) {
-                    min = Math.min(min, arr[j]);
+        for (int a = 0; a < queries.length; a++) {
+            int s = queries[a][0];
+            int e = queries[a][1];
+            int k = queries[a][2];
+            int minValue = Integer.MAX_VALUE;
+            for (int b = s; b <= e; b++) {
+                if (arr[b] > k) {
+                    minValue = Math.min(minValue, arr[b]);
                 }
             }
-            if (min != Integer.MAX_VALUE) {
-                list.add(min);
-            } else {
+            if (minValue == Integer.MAX_VALUE) {
                 list.add(-1);
+            } else {
+                list.add(minValue);
             }
+
         }
-        int[] answer = list.stream()
-                .mapToInt(i -> i)
-                .toArray();
+        int[] answer = list.stream().mapToInt(Integer::intValue).toArray();
         return answer;
     }
 }
