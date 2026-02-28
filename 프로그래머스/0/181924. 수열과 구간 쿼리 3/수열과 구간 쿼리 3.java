b43bccf43c16375eed75b246e5091e212032e021
HG-Yoon <younha3710@naver.com>
2026-02-28 15:01:06 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.06 ms, Memory: 84.5 MB -BaekjoonHub

--- 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
@@ -1,10 +1,12 @@
 class Solution {
     public int[] solution(int[] arr, int[][] queries) {
         int[] answer = {};
-        for (int i = 0; i < queries.length; i++) {
-            int temp = arr[queries[i][0]];
-            arr[queries[i][0]] = arr[queries[i][1]];
-            arr[queries[i][1]] = temp;
+        for (int a = 0; a < queries.length; a++) {
+            int i = queries[a][0];
+            int j = queries[a][1];
+            int tmp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = tmp;
         }
         answer = arr;
         return answer;
