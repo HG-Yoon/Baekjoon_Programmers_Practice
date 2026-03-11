f35f5193a5ca1ffc8b689cdecbc08389838a3375
HG-Yoon <younha3710@naver.com>
2026-03-11 23:28:41 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.29 ms, Memory: 82.5 MB -BaekjoonHub

--- 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.java
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.java
@@ -1,12 +1,10 @@
 class Solution {
     public int solution(int[] arr, int idx) {
-        int answer = 0;
-        for (int i = idx; i < arr.length; i++) {
-            if (arr[i] == 1) {
+        int answer = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (i >= idx && arr[i] == 1) {
                 answer = i;
                 break;
-            } else {
-                answer = -1;
             }
         }
         return answer;
