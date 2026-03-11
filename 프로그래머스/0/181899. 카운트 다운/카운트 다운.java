54326e5a038d6003c4a96d2d72651b34b11e9f15
HG-Yoon <younha3710@naver.com>
2026-03-11 23:19:36 +0900

    [level 0] Title: 카운트 다운, Time: 2.82 ms, Memory: 85.9 MB -BaekjoonHub

--- 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
@@ -3,13 +3,12 @@ import java.util.List;
 
 class Solution {
     public int[] solution(int start_num, int end_num) {
+        int[] answer = {};
         List<Integer> list = new ArrayList<>();
         for (int i = start_num; i >= end_num; i--) {
             list.add(i);
         }
-        int[] answer = list.stream()
-                .mapToInt(i -> i)
-                .toArray();
+        answer = list.stream().mapToInt(Integer::intValue).toArray();
         return answer;
     }
 }
