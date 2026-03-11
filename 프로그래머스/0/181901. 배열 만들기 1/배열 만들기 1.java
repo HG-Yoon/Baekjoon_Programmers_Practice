c043b6076ab9b2779f3878499dc014dd305db691
HG-Yoon <younha3710@naver.com>
2026-03-11 22:21:54 +0900

    [level 0] Title: 배열 만들기 1, Time: 13.14 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
@@ -1,19 +1,16 @@
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 class Solution {
     public int[] solution(int n, int k) {
+        int[] answer = {};
         List<Integer> list = new ArrayList<>();
         for (int i = 1; i <= n; i++) {
             if (i % k == 0) {
                 list.add(i);
             }
         }
-        Collections.sort(list);
-        int[] answer = list.stream()
-                .mapToInt(Integer::intValue)
-                .toArray();
+        answer = list.stream().mapToInt(Integer::intValue).toArray();
         return answer;
     }
 }
