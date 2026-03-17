e8a3ab0f5e98693de6cf1f25d766ddca4d3a387c
HG-Yoon <younha3710@naver.com>
2026-03-18 00:28:51 +0900

    [level 0] Title: 리스트 자르기, Time: 2.87 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
@@ -4,28 +4,28 @@ import java.util.List;
 class Solution {
     public int[] solution(int n, int[] slicer, int[] num_list) {
         int[] answer = {};
-        List<Integer> temp = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
         int a = slicer[0];
         int b = slicer[1];
         int c = slicer[2];
         if (n == 1) {
             for (int i = 0; i <= b; i++) {
-                temp.add(num_list[i]);
+                list.add(num_list[i]);
             }
         } else if (n == 2) {
             for (int i = a; i < num_list.length; i++) {
-                temp.add(num_list[i]);
+                list.add(num_list[i]);
             }
         } else if (n == 3) {
             for (int i = a; i <= b; i++) {
-                temp.add(num_list[i]);
+                list.add(num_list[i]);
             }
         } else if (n == 4) {
             for (int i = a; i <= b; i += c) {
-                temp.add(num_list[i]);
+                list.add(num_list[i]);
             }
         }
-        answer = temp.stream().mapToInt(Integer::intValue).toArray();
+        answer = list.stream().mapToInt(Integer::intValue).toArray();
         return answer;
     }
 }
