764b83991acfdf97594a718a6ff8ad690a6674f7
HG-Yoon <younha3710@naver.com>
2026-03-18 00:50:48 +0900

    [level 0] Title: 2의 영역, Time: 1.72 ms, Memory: 94.2 MB -BaekjoonHub

--- 프로그래머스/0/181894. 2의 영역/2의 영역.java
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.java
@@ -1,31 +1,22 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 class Solution {
     public int[] solution(int[] arr) {
         int[] answer = {};
-        List<Integer> temp = new ArrayList<>();
-        int start = -1;
-        int end = -1;
+        int first = -1, last = -1;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == 2) {
-                if (start == -1) {
-                    // 아직 start가 설정되지 않았다면
-                    start = i;
+                if (first == -1) {
+                    first = i;
                 }
-                end = i;
+                last = i;
             }
         }
-        if (start != -1 && end != -1) {
-            for (int i = start; i <= end; i++) {
-                temp.add(arr[i]);
-            }
-        } else if (start != -1 && end == -1) {
-            temp.add(2);
-        } else if (start == -1 && end == -1) {
-            temp.add(-1);
+        if (first == -1) {
+            answer = new int[]{-1};
+        } else {
+            answer = Arrays.copyOfRange(arr, first, last + 1);
         }
-        answer = temp.stream().mapToInt(Integer::intValue).toArray();
         return answer;
     }
 }
