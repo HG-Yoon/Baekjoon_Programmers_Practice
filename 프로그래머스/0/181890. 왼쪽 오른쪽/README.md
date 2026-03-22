92485fb7ba67bd003f226b23a1636506485dd331
HG-Yoon <younha3710@naver.com>
2026-03-23 00:51:52 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.03 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/README.md
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/README.md
@@ -1,10 +1,10 @@
 # [level 0] 왼쪽 오른쪽 - 181890 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181890?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181890) 
 
 ### 성능 요약
 
-메모리: 70.2 MB, 시간: 0.03 ms
+메모리: 76.9 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 12월 20일 01:36:11
+2026년 03월 23일 00:51:48
 
 ### 문제 설명
 
--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
@@ -3,15 +3,26 @@ import java.util.Arrays;
 class Solution {
     public String[] solution(String[] str_list) {
         String[] answer = {};
-        int idx = -1;
+        int lIdx = -1;
+        int rIdx = -1;
         for (int i = 0; i < str_list.length; i++) {
-            if (str_list[i].equals("l") && idx == -1) {
-                idx = i;
-                answer = Arrays.copyOfRange(str_list, 0, idx);
-            } else if (str_list[i].equals("r") && idx == -1) {
-                idx = i;
-                answer = Arrays.copyOfRange(str_list, i + 1, str_list.length);
+            if (str_list[i].equals("l")) {
+                lIdx = i;
+                break;
             }
+            if (str_list[i].equals("r")) {
+                rIdx = i;
+                break;
+            }
+        }
+        // lIdx만 찾은 경우 (rIdx == -1)
+        if (lIdx != -1 && rIdx == -1) {
+            answer = Arrays.copyOfRange(str_list, 0, lIdx);
+        } else if (rIdx != -1 && lIdx == -1) {
+            // rIdx만 찾은 경우 (lIdx == -1)
+            answer = Arrays.copyOfRange(str_list, rIdx + 1, str_list.length);
+        } else if (lIdx == -1 && rIdx == -1) {
+            answer = new String[]{};
         }
         return answer;
     }
