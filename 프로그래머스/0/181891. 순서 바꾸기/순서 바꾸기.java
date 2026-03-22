8645698494eaf670e0d75db39ccc0a96bbdb89bb
HG-Yoon <younha3710@naver.com>
2026-03-23 00:11:36 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.03 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = {};
+        int[] arr1 = Arrays.copyOfRange(num_list, n, num_list.length);
+        int[] arr2 = Arrays.copyOfRange(num_list, 0, n);
+        answer = new int[arr1.length + arr2.length];
+        System.arraycopy(arr1, 0, answer, 0, arr1.length);
+        System.arraycopy(arr2, 0, answer, arr1.length, arr2.length);
+        return answer;
+    }
+}
