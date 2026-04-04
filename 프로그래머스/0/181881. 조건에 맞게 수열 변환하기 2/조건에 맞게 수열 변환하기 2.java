b9d46c241c903d61a59af5bbcee25ad13d841d6a
HG-Yoon <younha3710@naver.com>
2026-04-04 21:03:13 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 2, Time: 59.63 ms, Memory: 127 MB -BaekjoonHub

--- 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.java
+++ 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.java
@@ -1,24 +1,26 @@
+import java.util.Arrays;
+
 class Solution {
     public int solution(int[] arr) {
         int answer = 0;
-        int x = 0;
+        int count = 0;
+        int[] newArr = new int[arr.length];
         while (true) {
-            boolean changed = false;
+            System.arraycopy(arr, 0, newArr, 0, newArr.length);
             for (int i = 0; i < arr.length; i++) {
                 if (arr[i] >= 50 && arr[i] % 2 == 0) {
                     arr[i] /= 2;
-                    changed = true;
                 } else if (arr[i] < 50 && arr[i] % 2 != 0) {
                     arr[i] = arr[i] * 2 + 1;
-                    changed = true;
                 }
             }
-            if (changed == false) {
+            if (Arrays.equals(arr, newArr)) {
+                answer = count;
                 break;
+            } else {
+                count++;
             }
-            x++;
         }
-        answer = x;
         return answer;
     }
 }
