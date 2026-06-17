176fead1317a27d07b669c6c97ded894b12a7104
HG-Yoon <younha3710@naver.com>
2026-06-17 13:48:32 +0900

    [level 0] Title: 문자열 곱하기, Time: 1.33 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.java
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.java
@@ -1,11 +1,9 @@
 class Solution {
     public String solution(String my_string, int k) {
         String answer = "";
-        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < k; i++) {
-            sb.append(my_string);
+            answer += my_string;
         }
-        answer = sb.toString();
         return answer;
     }
 }
