6f6908d8d191b494873b9afa286a60d5f86b4556
HG-Yoon <younha3710@naver.com>
2026-06-22 19:36:36 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.02 ms, Memory: 87.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int n) {
+        int answer = lcm(6, n) / 6;
+        return answer;
+    }
+
+    static int gcd(int a, int b) {
+        while (b != 0) {
+            int r = a % b;
+            a = b;
+            b = r;
+        }
+        return a;
+    }
+
+    static int lcm(int a, int b) {
+        return a * b / gcd(a, b);
+    }
+}
