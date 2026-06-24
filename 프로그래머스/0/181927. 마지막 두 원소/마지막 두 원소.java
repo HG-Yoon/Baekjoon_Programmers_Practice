449956a77760c2a13d5477a00743227491b0a7cf
HG-Yoon <younha3710@naver.com>
2026-06-25 00:50:10 +0900

    [level 0] Title: 마지막 두 원소, Time: 5.28 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -5,13 +5,17 @@ import java.util.stream.Collectors;
 class Solution {
     public int[] solution(int[] num_list) {
         int[] answer = {};
-        List<Integer> resultList = Arrays.stream(num_list).boxed().collect(Collectors.toList());
+        List<Integer> tmp_list = Arrays.stream(num_list)
+                .boxed()
+                .collect(Collectors.toList());
         if (num_list[num_list.length - 1] > num_list[num_list.length - 2]) {
-            resultList.add(num_list[num_list.length - 1] - num_list[num_list.length - 2]);
+            tmp_list.add(num_list[num_list.length - 1] - num_list[num_list.length - 2]);
         } else {
-            resultList.add(num_list[num_list.length - 1] * 2);
+            tmp_list.add(num_list[num_list.length - 1] * 2);
         }
-        answer = resultList.stream().mapToInt(Integer::intValue).toArray();
+        answer = tmp_list.stream()
+                .mapToInt(Integer::intValue)
+                .toArray();
         return answer;
     }
 }
