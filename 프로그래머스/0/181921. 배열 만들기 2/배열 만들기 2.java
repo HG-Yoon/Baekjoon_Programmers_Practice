084187587b0e36f78f066cc01597c6371d37566a
HG-Yoon <younha3710@naver.com>
2026-07-01 20:36:38 +0900

    [level 0] Title: 배열 만들기 2, Time: 44.09 ms, Memory: 119 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
@@ -2,29 +2,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
-    static boolean check(int x) {
-        while (x > 0) {
-            int digit = x % 10;
-            if (digit != 0 && digit != 5) {
-                return false;
-            }
-            x /= 10;
-        }
-        return true;
-    }
-
     public int[] solution(int l, int r) {
         int[] answer = {};
         List<Integer> list = new ArrayList<>();
         for (int i = l; i <= r; i++) {
-            if (check(i)) {
+            String iStr = String.valueOf(i);
+            boolean found = true;
+            for (int j = 0; j < iStr.length(); j++) {
+                if (iStr.charAt(j) != '0' && iStr.charAt(j) != '5') {
+                    found = false;
+                }
+            }
+            if (found) {
                 list.add(i);
             }
         }
         if (list.isEmpty()) {
             list.add(-1);
         }
-        answer = list.stream().mapToInt(Integer::intValue).toArray();
+        answer = list.stream()
+                .mapToInt(Integer::intValue)
+                .toArray();
         return answer;
     }
 }
